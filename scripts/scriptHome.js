bf7317bf04db661691382f6ba7b02031b07c5d45
shriaviator <32628818+shriaviator@users.noreply.github.com>
2021-03-29 09:46:39 +0530

    changing branch

--- scripts/scriptHome.js
+++ scripts/scriptHome.js
@@ -16,7 +16,7 @@ function myFunction() {
     modal.style.display = "none";
   });
 }
-
+/*To-Do-Js :*/
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
